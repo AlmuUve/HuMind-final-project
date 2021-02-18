3f3499cd8dfd793e34b451395c42d556441da87f
jagtrrz <jagutierrezc7@gmail.com>
2021-02-18 11:07:49 +0000

    added database and relationships

--- src/api/models.py
+++ src/api/models.py
@@ -4,15 +4,17 @@ from sqlalchemy import Table, Column, Integer, ForeignKey, String, DateTime, Dat
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    image = db.Column(db.String(250), unique=False, nullable=False)
-    description = db.Column(db.Text(1000), unique=False, nullable=False)
-    is_psicologyst = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=True)
+    password = db.Column(db.String(250))
+    is_active = db.Column(db.Boolean)
+    image = db.Column(db.String(250))
+    description = db.Column(db.Text)
+    is_psychologyst = db.Column(db.Boolean)
     user_company = db.relationship('User_company', lazy=True)
-    user_psicologyst = db.relationship('User_psicologyst', lazy=True)
+    user_psychologyst = db.relationship('User_psychologyst', lazy=True)
     
 
     def __repr__(self):
@@ -22,13 +24,16 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+
             # do not serialize the password, its a security breach
         }
 
 class User_company(db.Model):
+    __tablename__ = 'user_company'
+
     id = db.Column(db.Integer, primary_key=True)
-    company_name = db.Column(db.String(250), unique=False, nullable=False)
-    company_number = db.Column(db.String(80), unique=True, nullable=False)
+    company_name = db.Column(db.String(250))
+    company_number = db.Column(db.String(80), unique=True)
     User_Id = db.Column(db.Integer, db.ForeignKey("user.id"))
     search_workshop = db.relationship('Search_workshop', lazy=True)
 
@@ -43,58 +48,18 @@ class User_company(db.Model):
             # do not serialize the password, its a security breach
         }
     
-class User_psicologyst(db.Model):
+class User_psychologyst(db.Model):
+    __tablename__ = 'user_psychologyst'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), unique=False, nullable=False)
-    lastname = db.Column(db.String(250), unique=False, nullable=False)
-    identity_number = db.Column(db.String(250), unique=True, nullable=False)
-    association_number = db.Column(db.String(250), unique=True, nullable=False)
-    speciality = db.Column(db.String(250), unique=False, nullable=False)
+    name = db.Column(db.String(250))
+    lastname = db.Column(db.String(250))
+    identity_number = db.Column(db.String(250), unique=True)
+    association_number = db.Column(db.String(250), unique=True)
+    speciality = db.Column(db.String(250))
     User_Id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    workshop = db.relationship('workshop', lazy=True)
+    workshop = db.relationship('Workshop', lazy=True)
 
- 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
-
-class Search_workshop(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    duration = db.Column(db.String(250), unique=False, nullable=False)
-    max_price = db.Column(db.Float(), unique=False, nullable=False)
-    date = db.Column(db.Date(), unique=False, nullable=False)
-    max_people = db.Column(db.Intenger(), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    user_company_id = db.Column(db.Integer, db.ForeignKey("user_company.id"))
-    category_id = db.Column(db.Intenger(), db.ForeignKey("category.id"))
- 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
-
-class Workshop(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=False, nullable=False)
-    duration = db.Column(db.String(250), unique=False, nullable=False)
-    price = db.Column(db.Float(), unique=False, nullable=False)
-    date = db.Column(db.Date(), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    max_people = db.Column(db.Intenger(), unique=False, nullable=False)
-    description = db.Column(db.Text(1000), unique=False, nullable=False)
-    user_psicologyst_id = db.Column(db.Integer, db.ForeignKey("user_psicologyst.id"))
-    # category_id = db.Column(db.Intenger(), db.ForeignKey("category.id"))
  
     def __repr__(self):
         return '<User %r>' % self.username
@@ -107,9 +72,33 @@ class Workshop(db.Model):
         }
 
 class Category(db.Model):
+    __tablename__ = 'category'
+
     id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.String(250), unique=True, nullable=False)
-    # search_workshop = db.relationship('Search_workshop', lazy=True)
+    category_name = db.Column(db.String(250), unique=True)
+    search_workshop = db.relationship('Search_workshop', lazy=True)
+     
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Search_workshop(db.Model):
+    __tablename__ = 'search_workshop'
+
+    id = db.Column(db.Integer, primary_key=True)
+    duration = db.Column(db.String(250))
+    max_price = db.Column(db.Float)
+    date = db.Column(db.Date)
+    max_people = db.Column(db.Integer)
+    is_active = db.Column(db.Boolean)
+    user_company_id = db.Column(db.Integer, db.ForeignKey("user_company.id"))
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
  
     def __repr__(self):
         return '<User %r>' % self.username
@@ -121,11 +110,25 @@ class Category(db.Model):
             # do not serialize the password, its a security breach
         }
 
-class Workshop_has_category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    workshop_id =  db.Column(db.Integer, db.ForeignKey("workshop.id"))
-    category_id =  db.Column(db.Integer, db.ForeignKey("category.id"))
+workshop_has_category = db.Table('workshop_has_category',
+    db.Column('workshop_id', db.Integer, db.ForeignKey("workshop.id"), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey("category.id"), primary_key=True),
+)
 
+class Workshop(db.Model):
+    __tablename__ = 'workshop'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250))
+    duration = db.Column(db.String(250))
+    price = db.Column(db.Float)
+    date = db.Column(db.Date)
+    is_active = db.Column(db.Boolean)
+    max_people = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    user_psychologyst_id = db.Column(db.Integer, db.ForeignKey("user_psychologyst.id"))
+    category_info_1 = db.relationship("Category", secondary= workshop_has_category, lazy='subquery',
+        backref=db.backref("workshops", lazy=True))
  
     def __repr__(self):
         return '<User %r>' % self.username
@@ -136,3 +139,4 @@ class Workshop_has_category(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
