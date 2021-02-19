95d8ace7cbf0d5aaa1594b5d03345eddbb0e3368
jagtrrz <jagutierrezc7@gmail.com>
2021-02-19 12:09:07 +0000

    methods GET for psychologist users and combination of user and user_psychologist dictionaries

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,29 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+
 import "../../styles/home.scss";
+
+import { Profiletemplate } from "../component/profilecard.jsx";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const userPsychologist = [store.user, [store.userPsychologist]];
+
+	let profilePsychologist = userPsychologist.flat().map((item, index) => {
+		return (
+			<Profiletemplate
+				name={item.name}
+				lastname={item.lastname}
+				description={item.description}
+				key={index.toString()}
+			/>
+		);
+	});
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<h1>{profilePsychologist}</h1>
 		</div>
 	);
 };
