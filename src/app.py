5ce61081807c5fcaa7889a47c659e1056f412c30
jagtrrz <jagutierrezc7@gmail.com>
2021-02-19 16:06:41 +0000

    GET companies and try fix the issues

--- src/app.py
+++ src/app.py
@@ -49,77 +49,18 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
-@app.route('/user', methods=['POST'])
-def add_user():
-    body = request.get_json()
-    if body.get("email", None):
-        new_user = User(
-            email = body.get("email"),
-            password = body.get("password"),
-            description = body.get("description"),
-            is_psychologist = body.get("is_psychologist"),
-            is_active = body.get("is_active")
-        )
-        print("hola", new_user)
-        new_user.add()
-        return jsonify(new_user.to_dict()), 200
-    return "NO SE CREA NADA AMIGO MIO", 400
 
-@app.route('/user/<int:id>/psychologist', methods=['POST'])
-def add_user_psychologist(id):
-    body = request.get_json()
-    if body.get("association_number", None):
-        new_user = User_psychologist(
-            name = body.get("name"),
-            lastname = body.get("lastname"),
-            identity_number = body.get("identity_number"),
-            association_number = body.get("association_number"),
-            speciality = body.get("speciality"),
-            user_id = id,            
-        )
-        new_user.add()
-        return jsonify(new_user.to_dict()), 200
-    return "NO SE CREA NADA AMIGO MIO", 400
-
-@app.route('/user/<int:id>', methods=['GET'])
-def get_user_information(id):
+@app.route('/user/company/<int:id>', methods=['GET'])
+def get_user_company_information(id):
     user = User.get_by_id(id)
-    if user:
-        return jsonify(user.to_dict()), 200
-    else: "User nor found", 404
+    user_company = User_company.get_by_user_id(user.id)
+    return jsonify(user_company.to_dict()), 200
 
-@app.route('/user/<int:id>', methods=['PUT'])
-def update_user(id):
-    body = request.get_json()
-    user = User.update_single_user(body, id)
-    change_user = User.get_by_id(id)
-    return jsonify(change_user.to_dict())
-
-@app.route('/user/<int:id>/psychologist', methods=['PUT'])
-def update_psychologist_user(id):
-    body = request.get_json()
-    user = User_psychologist.update_psychologist_user(body, id)
-    change_user = User_psychologist.get_by_user_id(id)
-    return jsonify(change_user.to_dict())
-
-  
-@app.route('/user/<int:id>/company', methods=['PUT'])
-def update_company_user(id):
-    body = request.get_json()
-    user = User_company.update_company_user(body, id)
-    change_user = User_company.get_by_id(id)
-    return jsonify(change_user.to_dict())
-  
-  
-@app.route('/user/<int:id>', methods=['DELETE'])
-def delete_one_user(id):
-    User.delete_user(id)
-    return "User Deleted Successfully", 200
-
-@app.route('/user/<int:id>', methods=['DELETE'])
-def delete_one_user(id):
-    User.delete_user(id)
-    return "User Deleted Successfully", 200
+@app.route('/user/psychologist/<int:id>', methods=['GET'])
+def get_user_company_information(id):
+    user = User.get_by_id(id)
+    user_psychologist = User_psychologist.get_by_user_id(user.id)
+    return jsonify(user_psychologist.to_dict()), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
