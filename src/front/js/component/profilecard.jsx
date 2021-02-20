54567e0dfe898272c5e12c2888379a0c9117968b
jagtrrz <jagutierrezc7@gmail.com>
2021-02-20 19:43:18 +0000

    Component profile almost done, GET methods OK

--- src/front/js/component/profilecard.jsx
+++ src/front/js/component/profilecard.jsx
@@ -3,8 +3,6 @@ import { Context } from "../store/appContext";
 
 import PropTypes from "prop-types";
 
-import { Card } from "react-bootstrap";
-
 import { Link, useParams } from "react-router-dom";
 
 import "../../styles/home.scss";
@@ -12,26 +10,35 @@ import "../../styles/home.scss";
 export const Profiletemplate = props => {
 	const { store, actions } = useContext(Context);
 
+	const toUpperCase = str => {
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	};
+
 	return (
-		<Card className="mb-2" style={{ width: "18rem" }}>
-			<Card.Img className="cardTemplate" variant="top" />
-			<Card.Body>
-				<Card.Title className="titleCard">
-					{props.name} {props.lastname}
-				</Card.Title>
-				<Card.Text className="descriptionCard">{props.description}</Card.Text>
-				<div className="d-flex justify-content-between">
-					<button className="cardButton btn btn-outline-warning">More Details</button>
-					<button className="btn btn-outline-warning">
-						<i className="fas fa-heart" />
-					</button>
+		<div className="col-lg-4 col-sm-12 profileCard">
+			<div className="imgProfile">
+				<img
+					className="avatar"
+					src="https://image.freepik.com/vector-gratis/perfil-avatar-mujer-icono-redondo_24640-14042.jpg"
+				/>
+			</div>
+			<div className="bodyCard">
+				<div className="titleCard">
+					{toUpperCase(props.item.name)} {toUpperCase(props.item.lastname)}
 				</div>
-			</Card.Body>
-		</Card>
+				<div className="descriptionCard">
+					<p>{props.item.speciality}</p>
+					<p>{props.item.association_number}</p>
+					<p>{props.item.description}</p>
+				</div>
+				<div className="bottomCard">
+					<i className="fas fa-envelope fa-2x" href={props.item.email} />
+					<button className="btn btn-outline-warning">Edit Profile</button>
+				</div>
+			</div>
+		</div>
 	);
 };
 Profiletemplate.propTypes = {
-	name: PropTypes.string,
-	lastname: PropTypes.string,
-	description: PropTypes.string
+	item: PropTypes.object
 };
