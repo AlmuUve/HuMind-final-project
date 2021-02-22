05504152c7800ef85bd5efd087b545f4579edfcd
Rafaela Gonçalves <rafaelagcalves@gmail.com>
2021-02-22 11:20:08 +0000

    Main updated

--- src/app.py
+++ src/app.py
@@ -49,50 +49,8 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
-@app.route('/user', methods=['POST'])
-def add_user():
-    body = request.get_json()
-    if body.get("email", None):
-        new_user = User(
-            email = body.get("email"),
-            password = body.get("password"),
-            description = body.get("description"),
-            is_psychologist = body.get("is_psychologist"),
-            is_active = body.get("is_active")
-        )
-        print("hola", new_user)
-        new_user.add()
-        return jsonify(new_user.to_dict()), 200
-    return "NO SE CREA NADA AMIGO MIO", 400
 
-@app.route('/user/<int:id>/psychologist', methods=['POST'])
-def add_user_psychologist(id):
-    body = request.get_json()
-    if body.get("association_number", None):
-        new_user = User_psychologist(
-            name = body.get("name"),
-            lastname = body.get("lastname"),
-            identity_number = body.get("identity_number"),
-            association_number = body.get("association_number"),
-            speciality = body.get("speciality"),
-            user_id = id,            
-        )
-        new_user.add()
-        return jsonify(new_user.to_dict()), 200
-    return "NO SE CREA NADA AMIGO MIO", 400
 
-@app.route('/user/<int:id>', methods=['GET'])
-def get_user_information(id):
-    user = User.get_by_id(id)
-    if user:
-        return jsonify(user.to_dict()), 200
-    else: "User nor found", 404
-  
-  
-@app.route('/user/<int:id>', methods=['DELETE'])
-def delete_one_user(id):
-    User.delete_user(id)
-    return "User Deleted Successfully", 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
