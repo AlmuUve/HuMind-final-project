457db568c9bdba0658d57e57e183e48976e87831
jagtrrz <jagutierrezc7@gmail.com>
2021-02-22 14:20:40 +0000

    fixing issues

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-<<<<<<< HEAD
-// import { Home } from "./pages/home";
-import injectContext from "./store/appContext";
-
-// import { Navbar } from "./component/navbar";
-// import { Footer } from "./component/footer";
-import { DeleteButton } from "./component/deleteButton";
-=======
 import { Profilepsychologist } from "./pages/profilePsychologist";
 import injectContext from "./store/appContext";
 
@@ -16,7 +8,6 @@ import injectContext from "./store/appContext";
 import { Register } from "./pages/register";
 import injectContext from "./store/appContext";
 
->>>>>>> main
 
 //create your first component
 const Layout = () => {
@@ -25,15 +16,6 @@ const Layout = () => {
 	return (
 		<div className="d-flex flex-column h-100">
 			<BrowserRouter basename={basename}>
-<<<<<<< HEAD
-				<ScrollToTop>
-					{/* <Navbar /> */}
-					<Switch>
-						<Route exact path="/profile/1" component={DeleteButton} />
-					</Switch>
-					{/* <Footer /> */}
-				</ScrollToTop>
-=======
 				<Switch>
 					<Route exact path="/psychologist">
 						<Profilepsychologist />
@@ -45,7 +27,6 @@ const Layout = () => {
 						<h1>Not found!</h1>
 					</Route>
 				</Switch>
->>>>>>> main
 			</BrowserRouter>
 		</div>
 	);
