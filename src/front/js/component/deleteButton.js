05bced58afebb9c06447ba8760045390d77542c8
AlmuUve <avelazqc@gmail.com>
2021-02-22 09:33:10 +0000

    buttons created

---
+++ src/front/js/component/deleteButton.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+export const DeleteButton = () => (
+	<button className="deleteButton">
+		{" "}
+		<i className="far fa-trash-alt deleteButton" />{" "}
+	</button>
+);
