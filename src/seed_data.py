aeaee7e809434a7d9904b537c660a0c925762950
AlmuUve <avelazqc@gmail.com>
2021-02-25 13:57:44 +0000

    category seed data created

---
+++ src/seed_data.py
@@ -0,0 +1,44 @@
+
+data = {
+    "Category": [{
+        "id": 1,
+        "category_name": "Leadership"
+    },
+    {
+        "id": 2,
+        "category_name": "Positive Psychology"
+    },
+    {
+        "id": 3,
+        "category_name": "Well-being"
+    },
+    {
+        "id": 4,
+        "category_name": "Gender identity"
+    },
+    {
+        "id": 5,
+        "category_name": "Workplace well-being"
+    },
+    {
+        "id": 6,
+        "category_name": "Stress management"
+    },
+    {
+        "id": 7,
+        "category_name": "Sleep management"
+    },
+    {
+        "id": 8,
+        "category_name": "Time management"
+    },
+    {
+        "id": 9,
+        "category_name": "Emotion management"
+    },
+    {
+        "id": 10,
+        "category_name": "Motivational issues"
+    },
+    ]
+}
