cf96a40131911bfafb2c0f0805699f6ada445001
AlmuUve <avelazqc@gmail.com>
2021-02-26 16:07:57 +0000

    wonderful landing layout

--- src/front/js/component/landingButton.js
+++ src/front/js/component/landingButton.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export const LandingButton = () => (
 	<button className="landingButton">
-		Sign In! <i className="fas fa-angle-double-right signInArrows" />
+		Sign In!
+		{/* <i className="fas fa-angle-double-right signInArrows" /> */}
 	</button>
 );
