dc5af8f7bbaffd1db2d296381836224ac0d6ab7d
jagtrrz <jagutierrezc7@gmail.com>
2021-03-04 13:58:20 +0000

    Add route to heroku

--- src/app.py
+++ src/app.py
@@ -63,10 +63,19 @@ def sitemap():
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-@app.route('/contact')
-def prueba():
-   send_simple_message()
-   return "hemos mandado algo?", 200
+# any other endpoint will try to serve it like a static file
+@app.route('/<path:path>', methods=['GET'])
+def serve_any_other_file(path):
+    if not os.path.isfile(os.path.join(static_file_dir, path)):
+        path = 'index.html'
+    response = send_from_directory(static_file_dir, path)
+    response.cache_control.max_age = 0 # avoid cache memory
+    return response
+
+# @app.route('/contact')
+# def prueba():
+#    send_simple_message()
+#    return "hemos mandado algo?", 200
 
 
 @app.route('/login', methods=['POST'])
