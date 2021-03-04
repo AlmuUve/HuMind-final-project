cfe94d5bf27b86ab49568b2f32dd12b56160281c
jagtrrz <jagutierrezc7@gmail.com>
2021-03-04 12:03:43 +0000

    againnnnn

--- src/api/contact.py
+++ src/api/contact.py
@@ -2,18 +2,7 @@ import requests
 import mailchimp_transactional as MailchimpTransactional
 from mailchimp_transactional.api_client import ApiClientError
 
-mailchimp = MailchimpTransactional.Client('ZXvsK0dFVCIIJezATwyOsA')
-message = {
-    "from_email": "jagutierrezc7@gmail.com",
-    "subject": "Hello world",
-    "text": "Welcome to Mailchimp Transactional!",
-    "to": [
-      {
-        "email": "joseangelgutierrezcabrera@gmail.com",
-        "type": "to"
-      }
-    ]
-}
+
 
 def run():
   try:
@@ -31,7 +20,7 @@ def send_simple_message():
 		"https://api.mailgun.net/v3/sandbox4b8cc3724b9a47c39bcf992d24be4539.mailgun.org/messages",
 		auth=("api", "603a38d8c15f947008e05d5b263e7dcb-e49cc42c-2407b11a"),
 		data={"from": "Excited User <mailgun@sandbox4b8cc3724b9a47c39bcf992d24be4539.mailgun.org>",
-			"to": ["jagutierrezc7@gmail.com"],
+			"to": ["jagutierrezc7@gmail.com", "jose@"],
 			"subject": "Hello",
 			"text": "Testing some Mailgun awesomness!"})
   
