c3147d9caf391842f9d5681a2bb97eca3ff0c947
AlmuUve <avelazqc@gmail.com>
2021-03-05 16:25:00 +0000

    details fixed

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -16,7 +16,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 		actions: {
 			getWorkshops: () => {
-				fetch("https://3001-amber-jaguar-5k0jfiuy.ws-eu03.gitpod.io/user/psychologist/1/workshops").then(
+				fetch("https://3001-turquoise-termite-crb3zrev.ws-eu03.gitpod.io/user/psychologist/1/workshops").then(
 					async res => {
 						const response = await res.json();
 						setStore({ workshops: response });
