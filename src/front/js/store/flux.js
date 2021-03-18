661cc53530665a6898fb3e5ac868213e51c124bc
jagtrrz <jagutierrezc7@gmail.com>
2021-03-18 12:08:08 +0000

    no last one

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,6 @@
 import jwt_decode from "jwt-decode";
 
-const url = "https://3001-bronze-marmot-s1vgikxt.ws-eu03.gitpod.io";
+const url = "https://humind.herokuapp.com";
 
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
